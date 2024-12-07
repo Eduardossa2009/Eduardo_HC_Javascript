6894af307e4f29c3da3f70f71ba365a6098c5d85
Eduardossa2009 <eduardossa2009@gmail.com>
2024-12-07 11:04:39 +0000

    Aula 9

--- js/index(5).js
+++ js/index(5).js
@@ -1,24 +1,77 @@
 let alunos = [
-    { nome: "Alice", nota: 89},
-    { nome: "Bob", nota: 92},
-    { nome: "Charlie", nota: 78},
-    { nome: "Chris", nota: 50},
+    { nome: "Alice", nota: 89 },
+    { nome: "Bob", nota: 92 },
+    { nome: "Charlie", nota: 78 },
+    { nome: "Chris", nota: 50 }
 ];
-
-function calcularMediaNotas(alunosNotas) {
+ 
+function calculaMediaNotas() {
     let totalNotas = 0
-    for(let position = 0; position < alunosNotas.length; position++) {
-        
-        totalNotas = alunosNotas[position].nota + totalNotas
+    
+    for (let position = 0; position < alunos.length; position++) {
+        totalNotas = alunos[position].nota + totalNotas
     }
-    return totalNotas / alunosNotas.length
+    
+    return totalNotas / alunos.length
 }
-
-function adicionaAluno (estudantes, aluno, nota) {
-    estudantes.push({ nome: aluno, nota: nota })
+ 
+function updateTable(alunos) {
+    const tbody = document.querySelector(".tableBody");
+    const average = document.querySelector(".averageScore")
+ 
+    while (tbody.firstChild) {
+        tbody.removeChild(tbody.firstChild);
+    }
+ 
+    let tr;
+    let tdName;
+    let tdScore;
+    for (let position = 0; position < alunos.length; position++) {
+        tr = document.createElement("tr");
+        tdName = document.createElement("td");
+        tdScore = document.createElement("td");
+        tdName.appendChild(document.createTextNode(alunos[position].nome));
+        tdScore.appendChild(document.createTextNode(alunos[position].nota));
+        tbody.appendChild(tr);
+        tr.appendChild(tdName);
+        tr.appendChild(tdScore);
+    }
+    average.textContent = calculaMediaNotas(alunos);
 }
+ 
+ 
+function adicionaAluno(aluno, nota) {
+    alunos.push({ nome: aluno, nota: nota })
+}
+ 
+function removerAluno(nomeAluno) {
+    let index = alunos.findIndex((aluno) => aluno.nome === nomeAluno)
+    if (index !== -1) {
+        alunos.splice(index, 1)
+    }
+}
+ 
+removerAluno("Charlie");
+adicionaAluno("Kyle", 57);
+updateTable(alunos);
+ 
+const addStudentButton = document.querySelector("#addStudentButton");
+const studentName = document.querySelector("#newStudentName");
+const studentScore = document.querySelector("#newStudentScore");
 
-adicionaAluno(alunos, "Kyle", 57)
+const removeStudentButton = document.querySelector("#removeStudentButton");
+const removeStudentName = document.querySelector("#removeStudentName");
 
-document.write("A média da turma é de: ");
-document.write(calcularMediaNotas(alunos).toString());
+addStudentButton.addEventListener("click", function (event) {
+    if (studentName.value !== "" && studentScore.value !== "") {
+        adicionaAluno(studentName.value, Number(studentScore.value));
+        updateTable(alunos);
+    }
+});
+
+removeStudentButton.addEventListener("click", function (event) {
+    if (removeStudentName.value !== "") {
+        removerAluno(removeStudentName.value)
+        updateTable(alunos);
+    }
+});
